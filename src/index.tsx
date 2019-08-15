eb4e759c3ef27a83c485792cc4941a488b3b2c04
Sarah Nouh <sarah.nouh17@gmail.com>
2019-08-15 18:14:24 +0200

    Adding scss, bootstrap, react-router to project

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/**
+ * @Author: sarahnouh
+ * @Date:   2019-08-14
+ * @Last modified by:   sarahnouh
+ * @Last modified time: 2019-08-15
+ */
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import "./index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
